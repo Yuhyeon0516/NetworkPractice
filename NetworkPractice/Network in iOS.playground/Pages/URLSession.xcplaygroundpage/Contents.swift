81be665f8a92bc1a7a67be6cd00172034c7f87f9
Yuhyeon <120432007+Yuhyeon@users.noreply.github.com>
2022-12-31 18:19:33 +0900

    Add files via upload

---
+++ NetworkPractice/Network in iOS.playground/Pages/URLSession.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// configuration -> urlsession -> urlsessionTask
+let configuration = URLSessionConfiguration.default
+let session = URLSession(configuration: configuration)
+
+let url = URL(string: "https://api.github.com/users/Yuhyeon")!
+
+let task = session.dataTask(with: url) { data, response, error in
+    guard let httpReponse = response as? HTTPURLResponse, (200..<300).contains(httpReponse.statusCode) else {
+        print("--> response \(response)")
+        return
+    }
+    
+    guard let data = data else { return }
+    
+    let result = String(data: data, encoding: .utf8)
+    print(result)
+}
+
+task.resume()
+
+//: [Next](@next)
---
+++ NetworkPractice/Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
@@ -0,0 +1,74 @@
+//: [Previous](@previous)
+
+import Foundation
+import Combine
+
+enum NetworkError: Error {
+    case invalidRequest
+    case responseError(statusCode: Int)
+}
+
+struct GithubProfile: Codable {
+    let login: String
+    let avatarUrl: String
+    let htmlUrl: String
+    let followers: Int
+    let following: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatarUrl = "avatar_url"
+        case htmlUrl = "html_url"
+        case followers
+        case following
+    }
+}
+
+
+final class NetworkService {
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        session = URLSession(configuration: configuration)
+    }
+    
+    func fetchProfile(userName: String) -> AnyPublisher<GithubProfile, Error> {
+        let url = URL(string: "https://api.github.com/users/\(userName)")!
+        let publisher = session
+            .dataTaskPublisher(for: url)
+            //서버에서 받은 response 확인
+            .tryMap { result -> Data in
+                guard let httpResponse = result.response as? HTTPURLResponse,
+                      (200..<300).contains(httpResponse.statusCode) else {
+                    let response = result.response as? HTTPURLResponse
+                    let statusCode = response?.statusCode ?? -1
+                    throw NetworkError.responseError(statusCode: statusCode)
+                }
+                return result.data
+            }
+            //받은 data 디코딩
+            .decode(type: GithubProfile.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+        return publisher
+    }
+}
+
+let networkService = NetworkService(configuration: .default)
+
+let subscription = networkService
+    .fetchProfile(userName: "Yuhyeon")
+    .receive(on: RunLoop.main)
+    .sink { compeletion in
+        print("Compelition: \(compeletion)")
+    } receiveValue: { profile in
+        print("Profile: \(profile)")
+    }
+
+
+
+
+
+//: [Next](@next)
+
+
+
